a1b713636803bbc52a7a782a282b79ec59c9deff
eswoow <164808454+eswoow@users.noreply.github.com>
2024-08-11 19:47:33 -0300

    Gun + player health
    Some player stuff

--- Player.gd
+++ Player.gd
@@ -35,7 +35,7 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
+		
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
